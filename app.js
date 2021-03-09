b8ae2246a677ac985ed67fc8c6aaccd7d551a72a
Jackson Hsieh <jackson97042@gmail.com>
2021-03-10 00:08:03 +0800

    feat: add to cart feature postCart

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const logger = require('morgan')
 const exphbs = require('express-handlebars')
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 const PORT = process.env.PORT || 3000
-const app = express();
+const app = express()
 // view engine setup
 app.engine('hbs', exphbs({ 
   extname: '.hbs',
   defaultLayout: 'main',
   helpers: require('./config/handlebars-helpers')
 }))
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser(process.env.SESSION_SECRET))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  name: 'Jackson',
+  cookie: { maxAge: 80000 },
+  resave: false,
+  saveUninitialized: true,
+}))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404))
 });
 
 // error handler
@@ -38,8 +48,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
 app.listen(PORT, () => {
--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,11 +6,38 @@ const PAGE_OFFSET = 0;
 
 module.exports = {
   getCart: async (req, res) => {
-    let cart = await Cart.findOne({include: 'items'})
-    cart = cart.toJSON()
+    console.log(req.session)
+    let cart = await Cart.findByPk(req.session.cartId, { 
+      include: 'items' 
+    })
+
+    cart = cart ? cart.toJSON() : { items: [] }
     const totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b)=> a + b) : 0
       
-      return res.render('cart', { cart, totalPrice })
+    return res.render('cart', { cart, totalPrice })
+  },
+
+  postCart: async (req, res) => {
+    const [cart, ifCartCreated] = await Cart.findOrCreate({
+      where: { id: req.session.cartId || 0 } //if first time of user using the cart => req.session.cartId //undefined
+    })
     
+    const [cartItem, ifCartItemCreated] = await CartItem.findOrCreate({
+      where: {  
+        CartId: cart.id,
+        ProductId: req.body.productId 
+      }, 
+      default: {
+        CartId: cart.id,
+        ProductId: req.body.productId,
+      }
+    })
+
+    const updatedCartItem = await cartItem.update({ 
+      quantity: (cartItem.quantity || 0) + 1
+    })
+
+    req.session.cartId = cart.id
+    return req.session.save(() => res.redirect('back'))
   },
 }
